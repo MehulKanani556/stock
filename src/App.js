a8ab140b118e780cf72055135fa552082331f7ce
savanidhruvin <dhruvinsavani9313@gmail.com>
2025-04-08 09:21:02 +0530

    savani7-4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Layout from './Components/Layout.jsx';
 import Dashboard from './Components/Pages/Dashboard.jsx';
 import Product from './Components/Pages/Product.jsx';
 import RawMaterial from './Components/Pages/RawMaterial.jsx';
+import AddTaxRates from './Components/Pages/AddTaxRates.js';
+import FInancial_Setting_TaxRated from './Components/Pages/FInancial_Setting_TaxRated.js';
+import EditTaxRates from './Components/Pages/EditTaxRates.js';
+
 function App() {
   return (
   <div>
@@ -13,6 +17,9 @@ function App() {
            <Route path="dashboard" element={<Dashboard />} />
            <Route path="Product" element={<Product />} />
            <Route path="RawMaterial" element={<RawMaterial />} />
+           <Route path="taxrated" element={<FInancial_Setting_TaxRated/>} />
+           <Route path="addtaxrate" element={<AddTaxRates/>} />
+           <Route path="edittaxrate" element={<EditTaxRates/>} />
         </Route>
       </Routes>
     </div>
---
+++ src/Components/Pages/FInancial_Setting_TaxRated.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react'
+import Bar  from '../Images/Dhruvin/bar.svg'
+import Trash  from '../Images/Dhruvin/trash.svg'
+import Pen from '../Images/Dhruvin/pen.svg'
+import Red from '../Images/Dhruvin/trash_red.svg'
+import { useNavigate } from 'react-router-dom'
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+
+const FInancial_Setting_TaxRated = () => {
+
+const navigate = useNavigate()
+const [open, setOpen] = useState(false)
+
+  return (
+    <div>
+       <div className='bg-[#f5f5f5]'>
+          <div className='px-8 pt-7'>
+              <div className='flex justify-between items-center'>
+                 <div>
+                    <h2 className='text-d_color text-[24px] font-medium'>Tax Rates</h2>
+                    <p className='cursor-pointer'><span className='text-[#727272]'>Dashboard /</span> <span className='text-d_color'>Tax Rates</span></p>
+                 </div>
+                 <div className='flex'>
+                    <button className='ds_column_btn  flex me-4'> <img src={Bar} alt="" className='me-2' /> <span >Column</span></button>
+                    <button className='ds_column_btn  flex me-4'> <img src={Trash} alt="" className='me-2' /> <span >Delete All</span></button>
+                    <button className='ds_add_currency' onClick={()=> navigate("/layout/addtaxrate")}>+ Add Tax Rate</button>
+                 </div>
+              </div>
+              <div className='pt-7'>
+                  <div className='shadow-[1px_1px_20px_0.5px_rgba(0,0,0,0.06)] bg-white'>
+                    <div>
+                        <table className='w-full ds_currency_table'>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Tax Rates(%)</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>VAT</td>
+                                    <td>16</td>
+                                    <td>
+                                       <label className="inline-flex items-center me-5 cursor-pointer">
+                                            <input type="checkbox" value="" className="sr-only peer" defaultChecked />
+                                            <div className="relative w-10 h-5 bg-[#727272] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white peer-checked:after:start-[6px] after:content-[''] after:absolute after:top-0.5  after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all  peer-checked:bg-d_color dark:peer-checked:bg-red-600"></div>
+                                       </label>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <button onClick={()=> navigate("/layout/edittaxrate")} className='ds_crud_btn me-3'>
+                                                <img src={Pen} alt="" />
+                                            </button>
+                                            <button onClick={()=> setOpen(true)} className='ds_crud_btn'>
+                                                <img src={Red} alt="" />
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>TGST</td>
+                                    <td>18</td>
+                                    <td>
+                                       <label className="inline-flex items-center me-5 cursor-pointer">
+                                            <input type="checkbox" value="" className="sr-only peer" defaultChecked />
+                                            <div className="relative w-10 h-5 bg-[#727272] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white peer-checked:after:start-[6px] after:content-[''] after:absolute after:top-0.5  after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all  peer-checked:bg-d_color dark:peer-checked:bg-red-600"></div>
+                                       </label>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <button className='ds_crud_btn me-3'>
+                                                <img src={Pen} alt="" />
+                                            </button>
+                                            <button className='ds_crud_btn'>
+                                                <img src={Red} alt="" />
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>HST</td>
+                                    <td>12</td>
+                                    <td>
+                                       <label className="inline-flex items-center me-5 cursor-pointer">
+                                            <input type="checkbox" value="" className="sr-only peer" defaultChecked />
+                                            <div className="relative w-10 h-5 bg-[#727272] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white peer-checked:after:start-[6px] after:content-[''] after:absolute after:top-0.5  after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all  peer-checked:bg-d_color dark:peer-checked:bg-red-600"></div>
+                                       </label>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <button className='ds_crud_btn me-3'>
+                                                <img src={Pen} alt="" />
+                                            </button>
+                                            <button className='ds_crud_btn'>
+                                                <img src={Red} alt="" />
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                               
+                            </tbody>
+                        </table>
+                    </div>
+                  </div> 
+
+                  <Dialog open={open} onClose={setOpen} className="relative z-10">
+                      <DialogBackdrop transition className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"/>    
+                      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                          <DialogPanel transition className="relative transform overflow-hidden rounded-[5px] bg-red text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-xl data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                  <div className="mt-2">
+                                    <p className="text-lg text-center text-d_color">
+                                       Are you sure you want to delete
+                                       Tax Rates ?
+                                    </p>
+                                  </div>
+                              <div className='text-center mt-10 mb-6'>
+                                 <button type="button" className='ds_cancel_btn me-4' onClick={() => setOpen(false)}>Cancel
+                                 </button>
+                                 <button type="button" className='ds_add_btn' data-autofocus onClick={() => setOpen(false)}>Delete
+                                 </button>
+                              </div>
+                            </div>
+                          </DialogPanel>
+                        </div>
+                      </div>
+                 </Dialog>
+              </div>
+          </div>
+       </div>
+    </div>
+  )
+}
+
+export default FInancial_Setting_TaxRated
